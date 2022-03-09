0edd58c3c3a764080e3e746c3896e9bd79b5bbd3
ff <ff>
2022-03-09 21:24:19 +0800

    相机跟随

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerFollow;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - playerFollow.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerFollow.position + offset;
+        
+    }
+}
