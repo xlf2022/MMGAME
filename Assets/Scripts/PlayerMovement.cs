78445e649592c9d9ed1700a2449f304fd29a427a
ff <ff>
2022-03-09 20:37:20 +0800

    Init

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+
+    [SerializeField] private float moveSpeed;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        moveH = Input.GetAxis("Horizontal") * moveSpeed;
+        moveV = Input.GetAxis("Vertical") * moveSpeed;
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+
+    private void Flip()
+    {
+        if (moveH > 0) transform.eulerAngles = new Vector3(0, 0, 0);
+        if (moveH < 0) transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+}
+
