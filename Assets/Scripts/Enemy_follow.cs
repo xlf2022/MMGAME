c8438dc75b4f7a56d24b4de4f8809fc03fca7069
ff <ff>
2022-03-11 22:26:01 +0800

    怪物跟随

---
+++ Assets/Scripts/Enemy_follow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_follow : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Transform target;
+    [SerializeField] private float maxHp;
+    public float hp;
+
+    void Start()
+    {
+        hp = maxHp;
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+    }
+}
