dbd27f033b9c1332a745c7280dd1655ad5de063d
ff <ff>
2022-03-24 22:46:44 +0800

    血量实时显示

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,24 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float PmaxHp;//最大血量
-    public float Php;//当前hp
+    [SerializeField] public  float PmaxHp;//最大血量
+    public  float Php;//当前hp
 
     [HideInInspector]
     public bool isattacked;//是否变量做受伤检测
     void Start()
     {
+        
         Php = PmaxHp;
+       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void TakenDamage(float _amout)
---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] public  float HmaxHp;//最大血量
+    public  float Hhp;//当前hp
+
+    public PlayerHealth PH;
+
+    private Image Hbar;//控制图片
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hbar = GetComponent<Image>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HmaxHp = PH.PmaxHp;
+        Hhp = PH.Php;
+        Hbar.fillAmount =Hhp/HmaxHp;
+    }
+}
