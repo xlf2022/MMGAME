75cada82dc9174d2ffcd69fc633a1366b1a6ae64
ff <ff>
2022-03-24 21:33:51 +0800

    主角血量和受到敌人伤害效果

---
+++ Assets/Scripts/Enemy_damage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_damage : MonoBehaviour
+{
+    [SerializeField] private float minDamage, maxDamage;
+    private float attackDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)//主角碰撞检测
+    {
+        if (other.gameObject.tag == "Player")//通过标签触碰检测
+        {
+            Debug.Log("We have pengzhuang");
+            if (other.gameObject.GetComponent<PlayerHealth>().isattacked == false)
+            {
+
+                //造成伤害的函数
+                attackDamage = Random.Range(minDamage, maxDamage);
+                other.gameObject.GetComponent<PlayerHealth>().TakenDamage(attackDamage);
+
+
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float PmaxHp;//最大血量
+    public float Php;//当前hp
+
+    [HideInInspector]
+    public bool isattacked;//是否变量做受伤检测
+    void Start()
+    {
+        Php = PmaxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakenDamage(float _amout)
+    {
+        isattacked = true;
+        StartCoroutine(isAttackco());//调用下面的 isattackco函数
+        Php -= _amout;
+        if (Php <= 0)
+            Destroy(gameObject);
+    }
+
+    IEnumerator isAttackco()//每0.2秒后设定一次
+    {
+        yield return new WaitForSeconds(0.2f);
+        isattacked = false;
+    }
+
+}
