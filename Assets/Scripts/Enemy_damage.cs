bde3dfc95a275c6351eceee48c5c1f1ddce4ece1
ff <ff>
2022-03-25 22:31:21 +0800

    受伤闪烁

--- Assets/Scripts/Enemy_damage.cs
+++ Assets/Scripts/Enemy_damage.cs
@@ -34,4 +34,21 @@ public class Enemy_damage : MonoBehaviour
 
         }
     }
+    private void OnTriggerStay2D(Collider2D other)//主角碰撞检测
+    {
+        if (other.gameObject.tag == "Player")//通过标签触碰检测
+        {
+            Debug.Log("We have pengzhuang");
+            if (other.gameObject.GetComponent<PlayerHealth>().isattacked == false)
+            {
+
+                //造成伤害的函数
+                attackDamage = Random.Range(minDamage, maxDamage);
+                other.gameObject.GetComponent<PlayerHealth>().TakenDamage(attackDamage);
+
+
+            }
+
+        }
+    }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -8,6 +8,9 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] public  float PmaxHp;//最大血量
     public  float Php;//当前hp
+    public int Brknumbers;//受伤闪烁的次数
+    public float Btime;//闪烁的时间
+    private Renderer myRender;
 
     [HideInInspector]
     public bool isattacked;//是否变量做受伤检测
@@ -15,8 +18,8 @@ public class PlayerHealth : MonoBehaviour
     {
         
         Php = PmaxHp;
-       
 
+        myRender = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -25,19 +28,35 @@ public class PlayerHealth : MonoBehaviour
 
     }
 
-    public void TakenDamage(float _amout)
+    public void TakenDamage(float _amout)//收到伤害函数
     {
         isattacked = true;
         StartCoroutine(isAttackco());//调用下面的 isattackco函数
         Php -= _amout;
         if (Php <= 0)
-            Destroy(gameObject);
+        { Destroy(gameObject); }
+        BlinkPlayer(Brknumbers,Btime);
     }
 
-    IEnumerator isAttackco()//每0.2秒后设定一次
+    IEnumerator isAttackco()//每0.2秒后设定一次,无敌帧检测时间，这段时间收到伤害不会再受伤
     {
         yield return new WaitForSeconds(0.2f);
         isattacked = false;
+    }
+
+    void BlinkPlayer(int numblink,float seconds)
+    {
+        StartCoroutine(DoBlink(numblink, seconds));
+    }
+
+    IEnumerator DoBlink(int numblink, float seconds)
+    {
+        for (int i = 0; i < numblink; i++)
+        {
+            myRender.enabled = !myRender.enabled;
+            yield return new WaitForSeconds(seconds);
+        }
+        myRender.enabled = true;
     }
 
 }
