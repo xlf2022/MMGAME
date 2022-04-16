3fd5d67d6acdf29f6e87aa44d0a444a46936d3ec
ff <ff>
2022-04-16 17:37:44 +0800

    3级背包实例化

--- Assets/invetory/scripts/M2Inventory.cs
+++ Assets/invetory/scripts/M2Inventory.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class M1Inventory : MonoBehaviour
+public class M2Inventory : MonoBehaviour
 {
     public Inventory M1bag;//调用1级背包数据库，当前背包数据库
     public M1Slotdisplay M1Slotprefabs;//调用预制体格子里的Slotdisplay
@@ -13,10 +12,10 @@ public class M1Inventory : MonoBehaviour
     // private item newitem;//临时存放升级物体
     private int itemnomber;//临时存放的升级物体编号，作为一个开关，一般情况下为0
 
-    public M2Inventory M2inventory;//下一级的背包管理器
+    public M3Inventory M2inventory;//下一级的背包管理器
 
     // List<GameObject> itemss = new List<GameObject>();//新建一个itemss临时链表放，放itemss数据
-    
+
     void Start()
     {
         freshM1bag();
@@ -26,20 +25,20 @@ public class M1Inventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
 
 
-     }
-    public void CreatNewItem(item item,int i)//创建一个新物体
+
+    }
+    public void CreatNewItem(item item, int i)//创建一个新物体
     {
-       
-            M1Slotdisplay newitem = Instantiate(M1Slotprefabs, M1Slotprefabs.transform.position, Quaternion.identity);
-            newitem.gameObject.transform.SetParent(transform);
-            newitem.M1slotItem = item;
-            newitem.M1slotimage.sprite = item.itemImage;
-            newitem.M1slotsinfo = item.itemInfo;
-            newitem.M1slotNomber = item.itemNomuber;
-            newitem.gameObject.GetComponent<itemNO>().itemNomber = i;
+
+        M1Slotdisplay newitem = Instantiate(M1Slotprefabs, M1Slotprefabs.transform.position, Quaternion.identity);
+        newitem.gameObject.transform.SetParent(transform);
+        newitem.M1slotItem = item;
+        newitem.M1slotimage.sprite = item.itemImage;
+        newitem.M1slotsinfo = item.itemInfo;
+        newitem.M1slotNomber = item.itemNomuber;
+        newitem.gameObject.GetComponent<itemNO>().itemNomber = i;
 
     }
 
@@ -49,10 +48,10 @@ public class M1Inventory : MonoBehaviour
         itemnomber = 0;
         for (int i = 0; i < transform.childCount; i++)//删除每次添加的预制体
         {
-            if (transform.childCount == 0) 
+            if (transform.childCount == 0)
                 break;
             Destroy(transform.GetChild(i).gameObject);
-          
+
         }
 
         //**************************以下检索3合一升级**************************
@@ -62,9 +61,9 @@ public class M1Inventory : MonoBehaviour
         }
         for (int i = 0; i < M1bag.itemList.Count; i++)//迭代一下，每有一个重复添加一次计数
         {
-            if (M1bag.itemList.Contains(M1bag.itemList[i])==true)
+            if (M1bag.itemList.Contains(M1bag.itemList[i]) == true)
             {
-                M1bag.itemList[i].itemHeld = M1bag.itemList[i].itemHeld+ 1;
+                M1bag.itemList[i].itemHeld = M1bag.itemList[i].itemHeld + 1;
             }
         }
         for (int i = 0; i < M1bag.itemList.Count; i++)//检索有3个就删除物体
@@ -72,7 +71,7 @@ public class M1Inventory : MonoBehaviour
             if (M1bag.itemList[i].itemHeld == 3)
             {
                 itemnomber = M1bag.itemList[i].itemNextNomber;//临时编号得到下一级的编号
-               // newitem = M1bag.itemList[i];//找到赋予的那个item
+                                                              // newitem = M1bag.itemList[i];//找到赋予的那个item
                 M1bag.itemList.Remove(M1bag.itemList[i]);
                 i = i - 1;
             }
@@ -97,13 +96,13 @@ public class M1Inventory : MonoBehaviour
             }
 
         }
-    
+
         //***************************以下将临时newitem在SY2找到*******************//
 
 
-       for (int i = 0; i < M1bag.itemList.Count; i++)//检索背包列表，添加回去
+        for (int i = 0; i < M1bag.itemList.Count; i++)//检索背包列表，添加回去
         {
-            CreatNewItem(M1bag.itemList[i],i);
+            CreatNewItem(M1bag.itemList[i], i);
         }
 
     }
@@ -114,13 +113,10 @@ public class M1Inventory : MonoBehaviour
         {
             if (i == NO)
                 M1bag.itemList.Remove(M1bag.itemList[i]);
-           
+
             // M1bag.itemList[i].itemObject.SetActive(true);
         }
         freshM1bag();
     }
-
-
-    
 
 }
---
+++ Assets/invetory/scripts/M3Inventory.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M3Inventory : MonoBehaviour
+{
+    public Inventory M1bag;//调用1级背包数据库，当前背包数据库
+    public M1Slotdisplay M1Slotprefabs;//调用预制体格子里的Slotdisplay
+    public itemNO itemNO;//物品编号
+    // public Inventory SY2bag;//调用2级背包数据库
+    //public Inventory M2bag;//下一级的背包数据库
+    // private item newitem;//临时存放升级物体
+    private int itemnomber;//临时存放的升级物体编号，作为一个开关，一般情况下为0
+
+    // public M1Inventory M2inventory;//下一级的背包管理器
+
+
+    void Start()
+    {
+        freshM1bag();
+        itemnomber = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+    public void CreatNewItem(item item, int i)//创建一个新物体
+    {
+
+        M1Slotdisplay newitem = Instantiate(M1Slotprefabs, M1Slotprefabs.transform.position, Quaternion.identity);
+        newitem.gameObject.transform.SetParent(transform);
+        newitem.M1slotItem = item;
+        newitem.M1slotimage.sprite = item.itemImage;
+        newitem.M1slotsinfo = item.itemInfo;
+        newitem.M1slotNomber = item.itemNomuber;
+        newitem.gameObject.GetComponent<itemNO>().itemNomber = i;
+
+    }
+
+    public void freshM1bag()//刷新1级背包
+    {
+        // clearPool();
+        for (int i = 0; i < transform.childCount; i++)//删除每次添加的预制体
+        {
+            if (transform.childCount == 0)
+                break;
+            Destroy(transform.GetChild(i).gameObject);
+
+        }
+
+        for (int i = 0; i < M1bag.itemList.Count; i++)//检索背包列表，添加回去
+        {
+            CreatNewItem(M1bag.itemList[i], i);
+        }
+
+    }
+
+    public void delectitem(int NO)//删除背包1级里 对应编号的物件
+    {
+        for (int i = 0; i < M1bag.itemList.Count; i++)
+        {
+            if (i == NO)
+                M1bag.itemList.Remove(M1bag.itemList[i]);
+
+            // M1bag.itemList[i].itemObject.SetActive(true);
+        }
+        freshM1bag();
+    }
+}
