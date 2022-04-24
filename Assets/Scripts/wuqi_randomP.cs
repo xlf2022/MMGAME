0f1e3ecf3e8dfaeee7f83b374f4530004543a622
ff <ff>
2022-04-24 23:49:37 +0800

    藤蔓实例化

---
+++ Assets/Scripts/wuqi_randomP.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wuqi_randomP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform playerP;//玩家位置
+    private Vector3 pos;//暂时存储位置的pos
+    public float fanwei;//随机的范围正
+    public float fufanwei;//随机的范围负
+    public void getposition()
+    {
+        pos.x = playerP.position.x+ Random.Range(fufanwei, fanwei);
+        pos.y = playerP.position.y + Random.Range(fufanwei, fanwei);
+        pos.z = playerP.position.z;
+        // transform.position=
+        transform.position = pos;
+    }
+
+}
