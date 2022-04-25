8f059a38edd28c46c8c9927f8e49daf514a41af0
ff <ff>
2022-04-25 23:58:47 +0800

    随机化藤蔓

--- Assets/Scripts/wuqi_randomP.cs
+++ Assets/Scripts/wuqi_randomP.cs
@@ -7,12 +7,14 @@ public class wuqi_randomP : MonoBehaviour
     // Start is called before the first frame update
     public Transform playerP;//玩家位置
     private Vector3 pos;//暂时存储位置的pos
-    public float fanwei;//随机的范围正
-    public float fufanwei;//随机的范围负
+    public float X;//正x
+    public float fuX;
+    public float Y;//正y
+    public float fuY;
     public void getposition()
     {
-        pos.x = playerP.position.x+ Random.Range(fufanwei, fanwei);
-        pos.y = playerP.position.y + Random.Range(fufanwei, fanwei);
+        pos.x = playerP.position.x+ Random.Range(X, fuX);
+        pos.y = playerP.position.y + Random.Range(Y, fuY);
         pos.z = playerP.position.z;
         // transform.position=
         transform.position = pos;
