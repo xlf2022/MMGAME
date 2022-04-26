bebe977273cf081bdc8900b2566c41e9ef6594c1
ff <ff>
2022-04-26 22:38:00 +0800

    1-3藤蔓实例化

--- Assets/invetory/scripts/wuqijiance/check.cs
+++ Assets/invetory/scripts/wuqijiance/check.cs
@@ -6,11 +6,14 @@ public class check : MonoBehaviour
 {
     // Start is called before the first frame update
     public Inventory M1bag;
-    public item thisitem1;
+    public item thisitem1;//小剑数据
+    public item thisitem2;//藤蔓
     public GameObject myobject1;//小剑1
+    public GameObject myobject2;//藤蔓
     void Start()
     {
         myobject1.SetActive(false);
+        myobject2.SetActive(false);
     }
 
     // Update is called once per frame
@@ -29,6 +32,18 @@ public class check : MonoBehaviour
         {
             //Debug.Log("运行了不包含");
             myobject1.SetActive(false);
+        }
+
+
+        if (M1bag.itemList.Contains(thisitem2) == true)
+        {
+            //Debug.Log("运行了包含");
+            myobject2.SetActive(true);
+        }
+        if (M1bag.itemList.Contains(thisitem2) == false)
+        {
+            //Debug.Log("运行了不包含");
+            myobject2.SetActive(false);
         }
     }
 }
