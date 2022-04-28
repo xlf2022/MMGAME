30727cfb9c37b39b7e1f69c1c9fe48bdd5df82d3
ff <ff>
2022-04-28 23:25:25 +0800

    蘑菇怪2实例化

--- Assets/Scripts/Enemy_Creat.cs
+++ Assets/Scripts/Enemy_Creat.cs
@@ -6,6 +6,7 @@ public class Enemy_Creat : MonoBehaviour
 {
     public CountDownTest countdown;//导入倒计时文件,一开始恒定1200,20分钟
     public GameObject enemy1;//怪物1
+    public GameObject enemy2;//怪物2
     private bool iscreate1;//判定是否生成
     private Vector2 pos;
 
@@ -18,24 +19,35 @@ public class Enemy_Creat : MonoBehaviour
     void Update()
     {
         
-        if (countdown.totalTime2>0 )//倒计时,初始设定为0
+        if (countdown.totalTime2>1170 )//倒计时,初始设定为0,奇了怪了40秒才进行下一次
         {
             if (!iscreate1 )
             {
                 pos.x = transform.position.x + Random.Range(-10, 10);
                 pos.y = transform.position.y + Random.Range(-10, 10);
                 GameObject.Instantiate(enemy1,pos, Quaternion.identity);
-               iscreate1 =true;
+                iscreate1 =true;
                 StartCoroutine(iscreate());
             }
         }
-        
+        if (countdown.totalTime2 <= 1170)//倒计时,初始设定为0
+        {
+            if (!iscreate1)
+            {
+                pos.x = transform.position.x + Random.Range(-10, 10);
+                pos.y = transform.position.y + Random.Range(-10, 10);
+                GameObject.Instantiate(enemy2, pos, Quaternion.identity);
+                iscreate1 = true;
+                StartCoroutine(iscreate());
+            }
+        }
+
     }
 
     IEnumerator iscreate()//每1秒后设定一次
     {
         //Debug.Log("设定了");
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1f);
         iscreate1=false;
     }
 }
