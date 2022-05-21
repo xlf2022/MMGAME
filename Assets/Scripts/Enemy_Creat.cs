b4af60af26d991514435f480facb6be451fcb42d
ff <ff>
2022-05-21 23:02:26 +0800

    怪物更新

--- Assets/Scripts/Enemy_Creat.cs
+++ Assets/Scripts/Enemy_Creat.cs
@@ -13,6 +13,21 @@ public class Enemy_Creat : MonoBehaviour
     public float createtime2;//开始刷新时间
     public float freshtime2;//刷新时间
 
+    public GameObject enemy3;
+    public float createtime3;
+    public float freshtime3;
+
+    public GameObject enemy4;
+    public float createtime4;
+    public float freshtime4;
+
+    public GameObject enemy5;
+    public float createtime5;
+    public float freshtime5;
+
+    public GameObject enemy6;
+    public float createtime6;
+    public float freshtime6;
 
     private bool iscreate1;//判定是否生成
     private Vector2 pos;
@@ -25,8 +40,13 @@ public class Enemy_Creat : MonoBehaviour
 
     void Update()
     {
-        FreshEnemy(createtime1, createtime2,enemy1,freshtime1);//第一个是开始时间，第二个结束时间，第三个怪物，第四个刷新时间
-        FreshEnemy(createtime2, 0, enemy2, freshtime2);
+        FreshEnemy(createtime1, createtime2, enemy1,freshtime1);//第一个是开始时间，第二个结束时间，第三个怪物，第四个刷新时间
+        FreshEnemy(createtime2, createtime3, enemy2, freshtime2);
+        FreshEnemy(createtime3, createtime4, enemy3, freshtime3);
+        FreshEnemy(createtime4, createtime5, enemy4, freshtime4);
+        FreshEnemy(createtime5, createtime6, enemy5, freshtime5);
+        FreshEnemy(createtime6, 0, enemy6, freshtime6);
+
         // if (countdown.totalTime2>1170 )//倒计时,初始设定为0,奇了怪了40秒才进行下一次
         // {
         //     if (!iscreate1 )
