2ae77298a1ea30f7d37f6438764cf081b2d5072d
ff <ff>
2022-06-17 22:13:35 +0800

    实装竹子

---
+++ Assets/Scripts/wuqi/wuqi_baozha.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wuqi_baozha : MonoBehaviour
+{
+    public Animator anim;//动画组件
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("发生了触发器");
+        if (other.gameObject.tag == "Enemy")//通过标签触碰检测
+        {
+            anim.SetFloat("baozha",2);
+        }
+    }
+}
