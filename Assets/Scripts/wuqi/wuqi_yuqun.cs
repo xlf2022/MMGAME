334ec19209216adcf1e6d10dfab5a2122bc55d4b
ff <ff>
2022-06-18 21:38:46 +0800

    实装鲨鱼群

---
+++ Assets/Scripts/wuqi/wuqi_yuqun.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wuqi_yuqun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;//移动速度
+    public float juli;//移动距离
+    public int x;//x方向
+    public float huisu;//回收速度
+    public float Flytime;//飞行时间
+    private float NFlytime;//当前飞行时间
+
+    public Transform PlayP;//玩家位置
+    private Rigidbody2D r2d;//自己的rigbod组件
+    private Transform myP;//自己的位置
+    private float office;//当前位置Y的差距记录
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        r2d.velocity = new Vector2(x * speed, 0);
+        myP = GetComponent<Transform>();
+        PlayP = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        NFlytime = Flytime;
+        transform.position = new Vector3(PlayP.position.x, PlayP.position.y + transform.position.y, 0);
+        office = PlayP.position.y - transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        r2d.transform.position = new Vector3(transform.position.x, PlayP.position.y-office, 0);
+        if (r2d.velocity.x ==0)
+        {
+            if (transform.position.x < PlayP.position.x)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                r2d.velocity = new Vector2(-x * speed, 0);
+            }
+            if (transform.position.x > PlayP.position.x)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                r2d.velocity = new Vector2(x * speed, 0);
+            }
+        }
+
+
+        if (Mathf.Abs(myP.transform.position.x - PlayP.position.x) >=juli)
+        {
+
+            if (transform.position.x < PlayP.position.x)
+            { transform.eulerAngles = new Vector3(0, 180, 0);
+                r2d.velocity = new Vector2(-x * speed, 0);
+            }
+            if (transform.position.x > PlayP.position.x)
+            { transform.eulerAngles = new Vector3(0, 0, 0);
+                r2d.velocity = new Vector2(x * speed, 0);
+            }
+        }
+
+    }
+
+}
--- Assets/invetory/scripts/wuqijiance/check.cs
+++ Assets/invetory/scripts/wuqijiance/check.cs
@@ -18,6 +18,7 @@ public class check : MonoBehaviour
     public item thisitem10;//戒指武器
     public item thisitem11;//经验种子
     public item thisitem12;//竹子武器
+    public item thisitem13;//鲨鱼武器
 
     public item thisitem15;//冲击火球武器
 
@@ -33,6 +34,7 @@ public class check : MonoBehaviour
     public GameObject myobject10;//戒指武器
     public GameObject myobject11;//经验种子武器
     public GameObject myobject12;//竹子武器
+    public GameObject myobject13;//鲨鱼武器
 
     public GameObject myobject15;//冲击火球武器
 
@@ -51,6 +53,7 @@ public class check : MonoBehaviour
         myobject10.SetActive(false);
         myobject11.SetActive(false);
         myobject12.SetActive(false);
+        myobject13.SetActive(false);
         myobject15.SetActive(false);
     }
 
@@ -70,6 +73,7 @@ public class check : MonoBehaviour
         checkinbag(thisitem10, myobject10);
         checkinbag(thisitem11, myobject11);
         checkinbag(thisitem12, myobject12);
+        checkinbag(thisitem13, myobject13);
         checkinbag(thisitem15, myobject15);
     }
     public void checkinbag(item newitem,GameObject newgameobject)
