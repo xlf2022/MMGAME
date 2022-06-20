70d6c180995bdde5aa99bcc69c67b3ab48b80044
ff <ff>
2022-06-20 23:48:32 +0800

    配置怪物

--- Assets/Scripts/Enemy_Creat.cs
+++ Assets/Scripts/Enemy_Creat.cs
@@ -15,21 +15,46 @@ public class Enemy_Creat : MonoBehaviour
     public float createtime2;//开始刷新时间
     public float freshtime2;//刷新时间
 
-    public GameObject enemy3;
+    public GameObject enemy3;//150
     public float createtime3;
     public float freshtime3;
 
-    public GameObject enemy4;
+    public GameObject enemy4;//200
     public float createtime4;
     public float freshtime4;
 
-    public GameObject enemy5;
+    public GameObject enemy5;//250
     public float createtime5;
     public float freshtime5;
 
-    public GameObject enemy6;
+    public GameObject enemy6;//300
     public float createtime6;
     public float freshtime6;
+
+    public GameObject enemy7;//350
+    public float createtime7;
+    public float freshtime7;
+
+    public GameObject enemy8;//400
+    public float createtime8;
+    public float freshtime8;
+
+    public GameObject enemy9;//450
+    public float createtime9;
+    public float freshtime9;
+
+    public GameObject enemy10;//500
+    public float createtime10;
+    public float freshtime10;
+
+    public GameObject enemy11;//550
+    public float createtime11;
+    public float freshtime11;
+
+    public GameObject enemy12;//600
+    public float createtime12;
+    public float freshtime12;
+
 
     private bool iscreate1;//判定是否生成
     private Vector2 pos;
@@ -48,6 +73,12 @@ public class Enemy_Creat : MonoBehaviour
         FreshEnemy(createtime4, enemy4, freshtime4);
         FreshEnemy(createtime5, enemy5, freshtime5);
         FreshEnemy(createtime6, enemy6, freshtime6);
+        FreshEnemy(createtime7, enemy7, freshtime7);
+        FreshEnemy(createtime8, enemy8, freshtime8);
+        FreshEnemy(createtime9, enemy9, freshtime9);
+        FreshEnemy(createtime10, enemy10, freshtime10);
+        FreshEnemy(createtime11, enemy11, freshtime11);
+        FreshEnemy(createtime12, enemy12, freshtime12);
     }
 
     public void FreshEnemy(float Createtime, GameObject Enemy, float Freshtime)
