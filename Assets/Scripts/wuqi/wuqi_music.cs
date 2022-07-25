0943b8487188c67f07dabfb874473d8776562c25
ff <ff>
2022-07-25 23:13:46 +0800

    背景音乐和小剑音效

---
+++ Assets/Scripts/wuqi/wuqi_music.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wuqi_music : MonoBehaviour
+{
+    public AudioSource Mysource;
+    // Start is called before the first frame update
+
+    public void bofang()
+    {
+        Mysource.Play();
+    }
+}
