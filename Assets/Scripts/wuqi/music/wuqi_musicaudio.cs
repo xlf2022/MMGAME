f9590ee4af166b838828858c9a89c9ac31a799e8
ff <ff>
2022-07-28 21:29:32 +0800

    配置音乐

---
+++ Assets/Scripts/wuqi/music/wuqi_musicaudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wuqi_musicaudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource Mysource;
+    public string wuqiname;
+    void Start()
+    {
+        Mysource = GameObject.Find(wuqiname).GetComponent<AudioSource>();
+        Mysource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
