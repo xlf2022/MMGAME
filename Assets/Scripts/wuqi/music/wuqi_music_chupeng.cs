b7e8ede18ed6b5e3bd6a66364f05641ca24dd6ac
ff <ff>
2022-07-30 16:39:15 +0800

    配置音乐

---
+++ Assets/Scripts/wuqi/music/wuqi_music_chupeng.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wuqi_music_chupeng : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource Mysource;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")//通过标签触碰检测
+        {
+            Mysource.Play();
+        }
+    }
+}
