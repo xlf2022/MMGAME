670ade89918d4d450411671caf5f10000862c246
ff <ff>
2022-08-22 22:44:20 +0800

    修改数值

---
+++ Assets/nvshen/nvshen/Getiteminfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Getiteminfo : MonoBehaviour
+{
+    public item linshiitem;
+    // Start is called before the first frame update
+ 
+}
