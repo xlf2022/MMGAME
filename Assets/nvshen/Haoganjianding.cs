574bf795577754460f3b5dc2b2f1ba99458f5f16
ff <ff>
2022-10-20 23:28:11 +0800

    随机脱衣

--- Assets/nvshen/Haoganjianding.cs
+++ Assets/nvshen/Haoganjianding.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
 
 public class Haoganjianding : MonoBehaviour
 {
@@ -22,9 +23,35 @@ public class Haoganjianding : MonoBehaviour
 
     public Animator anmia;//兔兔动画控制
 
+    public SpriteResolver LXiong;//左胸配件
+
+    public int LXnumber;//左胸配件数量
+
+    public SpriteResolver RXiong;//右胸配件
+
+    public int RXnumber;//右胸配件数量
+
+    public SpriteResolver Qipao;//旗袍配件
+
+    public int Qipaonumber;//旗袍配件数量
+
+    public SpriteResolver Rhand;//右手配件
+
+    public int Rhandnumber;//右手配件数量
+
+    public SpriteResolver Shangbanshen;//上半身配件
+
+    public int Shangbanshennumber;//上半身配件数量
+
+    public SpriteResolver Shenti;//上半身配件
+
+    public int Shentinumber;//上半身配件数量
+
+    private int allnumber;//装备总数
+
     public void Start()
     {
-        
+        allnumber = LXnumber + RXnumber + Qipaonumber + Rhandnumber + Shangbanshennumber + Shentinumber-6;
     }
 
     public void Update()
@@ -37,22 +64,86 @@ public class Haoganjianding : MonoBehaviour
                 HaoGan.haoganJJ(add);
                 thisitem = null;
                 getitem.thisitem = null;
-                anmia.SetFloat("mean", 0);
+                anmia.SetFloat("mean", 0);//切换正常动画
             }
             if (tutuhate.itemList.Contains(thisitem))//如果该物品包含在讨厌的列表内
             {
                 HaoGan.haoganJJ(jian);
                 thisitem = null;
                 getitem.thisitem = null;
-                anmia.SetFloat("mean", 1);
+                anmia.SetFloat("mean", 1);//切换生气动画
+                tuoyi();
+
+
             }
-            //if (!tutulike.itemList.Contains(thisitem)&& !tutuhate.itemList.Contains(thisitem))
-           // { 
-          //      HaoGan.haoganJJ(pingchang);
-           //     thisitem = null;
-          //      getitem.thisitem = null;
-           //     anmia.SetFloat("mean", 0);
-          //  }
+         
+        }
+        jiance(LXiong,LXnumber);//检测左胸状态
+        jiance(RXiong, RXnumber);
+        jiance(Qipao, Qipaonumber);
+        jiance(Rhand, Rhandnumber);
+        jiance(Shangbanshen,Shangbanshennumber);
+        jiance(Shenti, Shentinumber);
+    }
+
+    public void jiance(SpriteResolver a, int c)
+    {
+        a.SetCategoryAndLabel(a.GetCategory(),c.ToString());
+    }
+
+    public void tuoyi()
+    {
+        if (allnumber != 0)
+        {
+            yuandian:
+            int random = Random.Range(0,6);
+            if (random == 0)
+            {
+                if (LXnumber == 1)
+                { goto yuandian; }
+                LXnumber--;
+                allnumber--;
+            }
+            if (random == 1 )
+            {
+                if (RXnumber == 1)
+                { goto yuandian; }
+                RXnumber --;
+                allnumber--;
+            }
+            if (random == 2 )
+            {
+                if (Qipaonumber == 1)
+                { goto yuandian; }
+                Qipaonumber--;
+                allnumber--;
+            }
+            if (random == 3)
+            {
+                if (Rhandnumber == 1)
+                { goto yuandian; }
+                Rhandnumber--;
+                allnumber--;
+            }
+            if (random == 4 )
+            {
+                if (Shangbanshennumber == 1)
+                { goto yuandian; }
+                Shangbanshennumber--;
+                allnumber--;
+            }
+            if (random == 5 )
+            {
+                if (Shentinumber == 1)
+                { goto yuandian; }
+                Shentinumber--;
+                allnumber--;
+            }
+
+
         }
     }
+
+
+
 }
