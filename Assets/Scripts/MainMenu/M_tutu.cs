792ca14ba68d78e4e2d0a848b21e55c8ebf67694
ff <ff>
2022-10-25 22:50:27 +0800

    loading图调整

---
+++ Assets/Scripts/MainMenu/M_tutu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class M_tutu : MonoBehaviour
+{
+    public Animator tutu;
+
+    private void Start()
+    {
+        tutu.speed = 0;
+    }
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnMouseEnter()
+    {
+        tutu.speed = 1;
+    }
+    private void OnMouseExit()
+    {
+        tutu.speed = 0;
+    }
+
+}
