3937bf3154ca6ed5295378b8cd91535c25dbcc6d
ff <ff>
2022-10-30 21:06:57 +0800

    调整游戏难度，loading黑影

--- Assets/Scripts/MainMenu/M_tutu.cs
+++ Assets/Scripts/MainMenu/M_tutu.cs
@@ -7,8 +7,14 @@ public class M_tutu : MonoBehaviour
 {
     public Animator tutu;
 
+    public List<SpriteRenderer> splist= new List<SpriteRenderer>();
+
     private void Start()
     {
+        foreach (var sp in splist)
+        {
+            sp.material.color = new Color((float)118/255, (float)118 / 255, (float)118 / 255);
+        }
         tutu.speed = 0;
     }
     private void OnMouseDown()
@@ -18,11 +24,19 @@ public class M_tutu : MonoBehaviour
 
     private void OnMouseEnter()
     {
+        foreach (var sp in splist)
+        {
+            sp.material.color = new Color(1, 1, 1);
+        }
         tutu.speed = 1;
     }
     private void OnMouseExit()
     {
         tutu.speed = 0;
+        foreach (var sp in splist)
+        {
+            sp.material.color = new Color((float)118 / 255, (float)118 / 255, (float)118 / 255);
+        }
     }
 
 }
