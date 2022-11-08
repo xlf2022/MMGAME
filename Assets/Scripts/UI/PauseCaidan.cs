2918a0b8297ef1cf7f941943d68ba37f70b7c8b4
ff <ff>
2022-11-08 22:20:28 +0800

    暂停菜单

---
+++ Assets/Scripts/UI/PauseCaidan.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class PauseCaidan : MonoBehaviour
+{
+    public GameObject caidan;
+    public PauseGame Pgame;
+    public AudioMixer audio1;
+    public Slider BgmSlider;
+    public Slider YinxiaoSlider;
+    public Slider PeiyinSlider;
+
+    private void Start()
+    {
+        caidan.SetActive(false);
+    }
+
+    public void OpenCaidan()
+    {
+        Pgame.Pausegame();
+        caidan.SetActive(true);
+    }
+
+    public void OffCaidan()
+    {
+        Pgame.Resumegame();
+        caidan.SetActive(false);
+    }
+    public void setbgm()//背景音乐
+    {
+        audio1.SetFloat("Bgm", BgmSlider.value);
+    }
+    public void setyinxiao()//音效音乐
+    {
+        audio1.SetFloat("YinXiao", YinxiaoSlider.value);
+    }
+    public void setpeiyin()//配音音乐
+    {
+        audio1.SetFloat("PeiYin", PeiyinSlider.value);
+    }
+
+
+}
